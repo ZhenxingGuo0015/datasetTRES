ea5cb223812f112bce11f49943df274e8ad3e52b
Zhenxing Guo <zhenxingguo@ZhenxingtekiMacBook-Pro.local>
2020-03-14 17:42:41 -0400

    add existing file

---
+++ data-raw/ProcessBam.R
@@ -0,0 +1,25 @@
+# preprocess of raw bam files
+read.BAM <- function(fn){
+  require(Rsamtools)
+  param = ScanBamParam(what=c("rname","strand","pos","qwidth"))
+  bam = scanBam(fn, param=param)[[1]]
+  ix = !is.na(bam$rname) & !is.na(bam$pos)
+  qwidth = bam$qwidth[ix]
+  IRange.reads <- GRanges(seqnames=Rle(bam$rname[ix]),
+                          ranges=IRanges(bam$pos[ix], width=bam$qwidth[ix]),
+                          strand=Rle(bam$strand[ix]))
+  IRange.reads
+}
+
+
+cb_6wk_input_rep1_chr1_chr19 = read.BAM("../inst/extdata/cb_6wk_input_rep1_chr1_chr19.bam")
+cb_6wk_ip_rep1_chr1_chr19 = read.BAM("../inst/extdata/cb_6wk_ip_rep1_chr1_chr19.bam")
+
+cb_6wk_input_rep2_chr1_chr19 = read.BAM("../inst/extdata/cb_6wk_input_rep2_chr1_chr19.bam")
+cb_6wk_ip_rep2_chr1_chr19 = read.BAM("../inst/extdata/cb_6wk_ip_rep2_chr1_chr19.bam")
+
+# ### use the data
+usethis::use_data(cb_6wk_input_rep1_chr1_chr19)
+usethis::use_data(cb_6wk_ip_rep1_chr1_chr19)
+usethis::use_data(cb_6wk_input_rep2_chr1_chr19)
+usethis::use_data(cb_6wk_ip_rep2_chr1_chr19)
